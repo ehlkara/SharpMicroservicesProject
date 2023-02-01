9967ca7881ca67ef818a3c84d31aae109a002259
ehlkara <ehlullah.fb@gmail.com>
2023-02-01 23:53:17 +0300

    added IdentityResourceOwnerValidator Service

--- IdentityServer/Sharp.IdentityServer/Startup.cs
+++ IdentityServer/Sharp.IdentityServer/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Sharp.IdentityServer.Services;
 
 namespace Sharp.IdentityServer
 {
@@ -54,6 +55,8 @@ namespace Sharp.IdentityServer
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
                 .AddAspNetIdentity<ApplicationUser>();
+
+            builder.AddResourceOwnerValidator<IdentityResourceOwnerValidator>();
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
