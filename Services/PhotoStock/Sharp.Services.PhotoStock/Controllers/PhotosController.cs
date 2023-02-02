ca959b7e49206a917579332f946a011910a24722
ehlkara <ehlullah.fb@gmail.com>
2023-02-02 17:45:49 +0300

    photoStock API finished

---
+++ Services/PhotoStock/Sharp.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sharp.Shared.ControllerBases;
+using Sharp.Shared.Dtos;
+using SharpCourse.Services.PhotoStock.Dtos;
+
+namespace SharpCourse.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if(photo != null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream, cancellationToken);
+                }
+
+                //http://www.photostock.api.com/photos/asdasd.jpg
+                var returnPath = "photos/" + photo.FileName;
+
+                PhotoDto photoDto = new() { Url = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
+        }
+
+
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+
+            if(!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
---
+++ Services/PhotoStock/Sharp.Services.PhotoStock/Dtos/PhotoDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SharpCourse.Services.PhotoStock.Dtos
+{
+	public class PhotoDto
+	{
+		public string Url { get; set; }
+	}
+}
+
