a96a3981eb81c01b87a18a4ec620a832a75b1552
ehlkara <ehlullah.fb@gmail.com>
2023-02-04 12:19:40 +0300

    created redisService and basketService

--- Services/Basket/SharpCourse.Services.Basket/Program.cs
+++ Services/Basket/SharpCourse.Services.Basket/Program.cs
@@ -1,10 +1,23 @@
-﻿using SharpCourse.Services.Basket.Settings;
+﻿using Microsoft.Extensions.Options;
+using SharpCourse.Services.Basket.Services;
+using SharpCourse.Services.Basket.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+    var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+
+    var redis = new RedisService(redisSettings.Host, redisSettings.Port);
+
+    redis.Connect();
+
+    return redis;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Services/Basket/SharpCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Sharp.Shared.Dtos;
+using SharpCourse.Services.Basket.Dtos;
+
+namespace SharpCourse.Services.Basket.Services
+{
+	public interface IBasketService
+	{
+		Task<Response<BasketDto>> GetBasket(string userId);
+
+		Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+
+		Task<Response<bool>> Delete(string userId);
+	}
+}
+
