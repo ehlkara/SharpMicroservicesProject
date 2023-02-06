bf5665b23ba5e8ce8eaa705d3d35c8b8e092698b
ehlkara <ehlullah.fb@gmail.com>
2023-02-06 18:16:26 +0300

    created discountService

--- Services/Discount/SharpCourse.Services.Discount/Services/IDiscountService.cs
+++ Services/Discount/SharpCourse.Services.Discount/Services/IDiscountService.cs
@@ -6,7 +6,7 @@ namespace SharpCourse.Services.Discount.Services
 	public interface IDiscountService
 	{
 		Task<Response<List<Models.Discount>>> GetAll();
-		Task<Response<Models.Discount>> GetById();
+		Task<Response<Models.Discount>> GetById(int id);
 		Task<Response<NoContent>> Save(Models.Discount discount);
 		Task<Response<NoContent>> Update(Models.Discount discount);
 		Task<Response<NoContent>> Delete(int id);
