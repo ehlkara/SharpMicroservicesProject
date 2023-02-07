6a9e87b27eed36819c179f7745dd745e7ae195dc
ehlullah karakurt <ehlullah.fb@gmail.com>
2023-02-07 16:06:07 +0300

    added mapping with autoMapper library

---
+++ Services/Order/SharpCourse.Services.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SharpCourse.Services.Order.Domain.OrderAggregate;
+using SharpCourse.Services.Services.Order.Application.Dtos;
+
+namespace SharpCourse.Services.Services.Order.Application.Mapping
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+        }
+    }
+}
