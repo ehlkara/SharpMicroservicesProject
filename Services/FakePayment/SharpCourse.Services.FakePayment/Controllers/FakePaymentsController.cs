07489ff4e3560f3b0554d031575a7c7fb0704d2a
ehlkara <ehlullah.fb@gmail.com>
2023-02-12 18:52:03 +0300

    updated dll

---
+++ Services/FakePayment/SharpCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sharp.Shared.ControllerBases;
+using Sharp.Shared.Dtos;
+
+namespace SharpCourse.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance<NoContent>(Response<NoContent>.Success(200));
+        }
+    }
+}
+
