7652083e0eff988f5a8904290707c2ab2b071fd0
ehlkara <ehlullah.fb@gmail.com>
2023-02-16 18:59:02 +0300

    created IIdentityService

---
+++ Frontends/SharpCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using IdentityModel.Client;
+using Sharp.Shared.Dtos;
+using SharpCourse.Web.Models;
+
+namespace SharpCourse.Web.Services.Interfaces
+{
+	public interface IIdentityService
+	{
+		Task<Response<bool>> SignIn(SigninInput signinInput);
+
+		Task<TokenResponse> GetAccessTokenByRefreshToken();
+
+		Task RevokeRefreshToken();
+	}
+}
+
