a833aed5f31f798fc09407528a42a68125d0f15b
ehlkara <ehlullah.fb@gmail.com>
2023-02-16 18:41:28 +0300

    created web project

---
+++ Frontends/SharpCourse.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpCourse.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
