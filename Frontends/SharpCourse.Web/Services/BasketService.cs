3b98a83bb1a2eac8b38e4f1bcc581a608376c3cc
ehlkara <ehlullah.fb@gmail.com>
2023-02-27 19:16:53 +0300

    completion services in basketService

--- Frontends/SharpCourse.Web/Services/BasketService.cs
+++ Frontends/SharpCourse.Web/Services/BasketService.cs
@@ -10,11 +10,13 @@ namespace SharpCourse.Web.Services
     {
         private readonly HttpClient _httpClient;
         private readonly ISharedIdentityService _sharedIdentityService;
+        private readonly IDiscountService _discountService;
 
-        public BasketService(HttpClient httpClient, ISharedIdentityService sharedIdentityService)
+        public BasketService(HttpClient httpClient, ISharedIdentityService sharedIdentityService, IDiscountService discountService)
         {
             _httpClient = httpClient;
             _sharedIdentityService = sharedIdentityService;
+            _discountService = discountService;
         }
 
         public async Task AddBasketItem(BasketItemViewModel basketItemViewModel)
@@ -38,14 +40,37 @@ namespace SharpCourse.Web.Services
             await SaveOrUpdate(basket);
         }
 
-        public Task<bool> ApplyDiscount(string discountCode)
+        public async Task<bool> ApplyDiscount(string discountCode)
         {
-            throw new NotImplementedException();
+            await CancelApplyDiscount();
+
+            var basket = await Get();
+            if (basket == null || basket.DiscountCode == null)
+                return false;
+
+            var hasDiscount = await _discountService.GetDiscount(discountCode);
+
+            if (hasDiscount == null)
+                return false;
+
+            basket.DiscountRate = hasDiscount.Rate;
+            basket.DiscountCode = hasDiscount.Code;
+            await SaveOrUpdate(basket);
+            return true;
         }
 
-        public Task<bool> CancelApplyDiscount()
+        public async Task<bool> CancelApplyDiscount()
         {
-            throw new NotImplementedException();
+            var basket = await Get();
+
+            if (basket == null || basket.DiscountCode == null)
+                return false;
+
+            basket.DiscountCode = null;
+
+            await SaveOrUpdate(basket);
+
+            return true;
         }
 
         public async Task<bool> Delete()
