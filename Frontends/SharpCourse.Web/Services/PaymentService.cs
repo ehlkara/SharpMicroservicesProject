801646892e96ae79020191e08549405a67a9b8fd
ehlkara <ehlullah.fb@gmail.com>
2023-03-01 19:56:06 +0300

    wrote paymentService

--- Frontends/SharpCourse.Web/Services/PaymentService.cs
+++ Frontends/SharpCourse.Web/Services/PaymentService.cs
@@ -6,11 +6,18 @@ namespace SharpCourse.Web.Services
 {
     public class PaymentService : IPaymentService
     {
+        private readonly HttpClient _httpClient;
 
-
-        public Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+        public PaymentService(HttpClient httpClient)
         {
-            throw new NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+        {
+            var response = await _httpClient.PostAsJsonAsync<PaymentInfoInput>("fakepayments", paymentInfoInput);
+
+            return response.IsSuccessStatusCode;
         }
     }
 }
