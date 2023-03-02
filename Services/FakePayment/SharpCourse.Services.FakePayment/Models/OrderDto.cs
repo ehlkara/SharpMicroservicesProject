efafbf4e58da2bc3b196e119797316c0d6f5a4b6
ehlkara <ehlullah.fb@gmail.com>
2023-03-02 13:52:45 +0300

    Updated paymentDto in FakePaymentService

---
+++ Services/FakePayment/SharpCourse.Services.FakePayment/Models/OrderDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace SharpCourse.Services.FakePayment.Models
+{
+	public class OrderDto
+	{
+        public OrderDto()
+        {
+            OrderItems = new List<OrderItemDto>();
+        }
+
+        public string BuyerId { get; set; }
+
+        public List<OrderItemDto> OrderItems { get; set; }
+
+        public AddressDto Address { get; set; }
+    }
+
+    public class AddressDto
+    {
+        public string District { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string Line { get; set; }
+    }
+
+    public class OrderItemDto
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
--- Services/FakePayment/SharpCourse.Services.FakePayment/Models/PaymentDto.cs
+++ Services/FakePayment/SharpCourse.Services.FakePayment/Models/PaymentDto.cs
@@ -8,6 +8,7 @@ namespace SharpCourse.Services.FakePayment.Models
         public string Expiration { get; set; }
         public string CVV { get; set; }
         public decimal TotalPrice { get; set; }
+        public OrderDto Order { get; set; }
     }
 }
 
